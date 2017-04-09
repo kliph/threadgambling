a54b6076a3e03e8668230d241393f946ea11a1e6
Cliff Rodgers <kliph@users.noreply.github.com>
2017-04-09 17:09:07 -0400

    First crack at signin

--- src/threadgambling/auth.cljs
+++ src/threadgambling/auth.cljs
@@ -1,10 +1,24 @@
 (ns threadgambling.auth
   (:require [threadgambling.state :as s]))
 
+(defn onSignIn [google-user]
+  (let [profile (.getBasicProfile google-user)]
+    (js/console.log (str "ID: " (.getId profile)))
+    (js/console.log (str "Name: " (.getName profile)))
+    (js/console.log (str "Email: " (.getEmail profile)))
+    (js/console.log (str "Profile: " profile))))
+
+(defn onSignOut []
+  (let [auth2 (.getAuthInstance gapi/auth2)]
+    (.then (.signOut auth2)
+           (fn [] (js/console.log "User Signed out")))))
+
 (defn sign-in-page []
   [:div#sign-in-page
-   [:a {:on-click #(swap! s/app-state assoc
-                          :signed-in true)
-        :href "/#/sign-in"}
-    [:img {:src "https://developers.google.com/identity/images/btn_google_signin_light_normal_web.png"
-           :alt "Sign in with google"}]]])
+   ;; [:a {:on-click #(swap! s/app-state assoc
+   ;;                        :signed-in true)
+   ;;      :href "/#/sign-in"}]
+
+   [:div {:className "g-signin2"
+          :data-onsuccess "onSignIn"}]
+   [:a {:on-click (fn [] (onSignOut))} "Sign out"]])
